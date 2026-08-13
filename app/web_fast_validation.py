056e0ca1bcbf29b7a6b0e826bf0a089318744587
LuisCarlos0306 <luisdeoliveira2708@gmail.com>
2026-08-13 19:11:24 -0300

    chore: alinhar versão da UI

--- app/web_fast_validation.py
+++ app/web_fast_validation.py
@@ -9,7 +9,7 @@ from fastapi.responses import HTMLResponse
 from app.web import UI_DIR, _require_access
 
 
-ASSET_VERSION = "1.30.26"
+ASSET_VERSION = "1.30.27"
 
 
 def _bool(name: str, default: bool) -> bool:
